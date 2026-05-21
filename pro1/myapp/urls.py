5c4106a16cc3906db372e23afdc08211e7d6a197
Denish Makvana <tamaro-email@gmail.com>
2026-05-21 17:18:13 +0530

    Added note create and display feature

--- pro1/myapp/urls.py
+++ pro1/myapp/urls.py
@@ -5,6 +5,7 @@ from .import views
 urlpatterns = [
   
     path('noteapp_view/',views.noteapp_view,name='noteapp_view'),
+    path('delete/<int:id>/',views.delete,name='delete'),
 
 
 ]
--- pro1/myapp/views.py
+++ pro1/myapp/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import noteapp
 
 # Create your views here.
 
 def noteapp_view(request):
-    note = noteapp.objects.all()
+        if request.method == 'POST':
+            title=request.POST.get('title')
+            description=request.POST.get('description')
+            noteapp.objects.create(title=title,description=description)
 
-    return render(request,"noteapp.html",{'note':note})
+            return redirect('/noteapp_view/')
+        
+        notes = noteapp.objects.all()
+        return render(request,"noteapp.html",{'notes':notes})
+
+
+def delete(request,id):
+      
+      notes=noteapp.objects.get(id=id)
+      notes.delete()
+      return redirect('/noteapp_view/')
+      
